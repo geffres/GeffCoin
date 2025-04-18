f5f5535949b94e64aec5455be1d8d6b69d8e934e
geffres <geffres32@gmail.com>
2025-04-17 23:16:19 -0400

    Update GeffCoin.sol
    Updated GeffCoin total suppy to 4,201,337 and removed airdrop

--- GeffCoin.sol
+++ GeffCoin.sol
@@ -6,13 +6,9 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract GeffCoin is ERC20, Ownable {
     constructor() ERC20("GeffCoin", "GEFF") {
-        _mint(msg.sender, 42001337 * (10 ** decimals()));
+        _mint(msg.sender, 4_201_337 * 10 ** decimals());
     }
 
-    /**
-     * @notice Burn a specific amount of tokens from the sender's wallet
-     * @param amount The amount of tokens to burn (including decimals)
-     */
     function burn(uint256 amount) external {
         _burn(msg.sender, amount);
     }
